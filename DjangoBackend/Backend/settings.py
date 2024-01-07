0393b5b8589b80eb677279526fa7a81f76ec6aa9
utibe solomon <utibesolomon6@gmail.com>
2024-01-07 21:13:37 +0100

    made some changes

--- DjangoBackend/Backend/settings.py
+++ DjangoBackend/Backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k5_@a&&!qrzj=j)gk2*v&tigy-r98!^v@#rxtm55qvqw$$=+m7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
