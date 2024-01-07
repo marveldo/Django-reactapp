a58e5f7996a31adb949466efc9b573e3588d0f43
utibe solomon <utibesolomon6@gmail.com>
2024-01-07 20:16:10 +0100

    changes in settings

--- DjangoBackend/Backend/settings.py
+++ DjangoBackend/Backend/settings.py
@@ -134,10 +134,21 @@ SIMPLE_JWT = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Winnermarvelous23@#$',
+        'HOST': 'db.mmhiamzphrccutigicsf.supabase.co',   # Set to the appropriate host if not using a local database
+        'PORT': '5432',        # Default PostgreSQL port
     }
 }
 
